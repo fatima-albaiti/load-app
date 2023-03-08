c24bfd3af23b955b8b78d6465e58b4db8019a5cc
fatima-albaiti <fatima@faras.ae>
2023-03-08 23:23:22 +0400

    add button to return to main activity

--- app/src/main/java/com/udacity/DetailActivity.kt
+++ app/src/main/java/com/udacity/DetailActivity.kt
@@ -1,5 +1,6 @@
 package com.udacity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -14,6 +15,12 @@ class DetailActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         binding.contentDetail.fileName.text = intent.getStringExtra(MainActivity.DOWNLOAD_NAME)?:""
         binding.contentDetail.fileStatus.text = intent.getStringExtra(MainActivity.DOWNLOAD_STATUS)?:""
+        binding.contentDetail.btnOk.setOnClickListener {
+            val intent = Intent(applicationContext, MainActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NO_HISTORY or Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
+            startActivity(intent)
+        }
     }
 
 }
